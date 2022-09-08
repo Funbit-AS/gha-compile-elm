bdbf0aa785c9d9d8b96fddd998328d8e839bdaf5
MauricioFUNBIT <111439956+MauricioFUNBIT@users.noreply.github.com>
2022-09-08 11:36:32 +0000

    modified script

--- compile.sh
+++ compile.sh
@@ -2,9 +2,9 @@ echo "Runnig compile.sh"
 
 #npm install --location=global elm@0.19.1 uglify-js
 
-echo "received:" ${INPUTS_ELM_PATHS} "as input"
+echo "received:" ${INPUT_ELM_PATHS} "as input"
 
-filepaths=${INPUTS_ELM_PATHS}
+filepaths=${INPUT_ELM_PATHS}
 
 echo "Current filepaths:" $filepaths
 
@@ -17,7 +17,7 @@ echo "Finished parsing filepaths"
 echo "Paths:"
 for path in "${paths[@]}";
 do
-    cd ${INPUTS_GITHUB_WORKSPACE}/wagtail/${INPUTS_PROJECT_NAME}/${path}/elm
+    cd ${INPUT_GITHUB_WORKSPACE}/wagtail/${INPUT_PROJECT_NAME}/${path}/elm
     echo pwd
     elm make src/FirstHomePage.elm --output elm.js
     #make release
