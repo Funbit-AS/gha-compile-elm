bc78f51ea8384060550782e5cb6ee972d15b5245
MauricioFUNBIT <111439956+MauricioFUNBIT@users.noreply.github.com>
2022-09-09 07:52:47 +0000

    First Cleanup

--- compile.sh
+++ compile.sh
@@ -1,20 +1,9 @@
-echo "Runnig compile.sh"
-
 npm install --location=global elm@0.19.1 uglify-js
-
-echo "received:" ${Elm_Path} "as input"
 
 filepaths=${Elm_Path}
 
-echo "Current filepaths: "$filepaths
-
-echo "Trying to parse filepaths"
-
 read -a paths <<< "$filepaths"
 
-echo "Finished parsing filepaths"
-
-echo "Paths:"
 for path in "${paths[@]}";
 do
     cd ${Workspace}/wagtail/${Project_Name}/${path}/elm
